ab14747b09485b461f09519cb3b71e699d2dc9b2
hdtuan87 <hdtuan87@gmail.com>
2012-07-17 02:59:58 +0000

    update LoginActivity và MainActivity

--- src/att/android/activity/LoginActivity.java
+++ src/att/android/activity/LoginActivity.java
@@ -6,21 +6,52 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements OnItemClickListener,
+		OnClickListener {
+	private EditText edtxtUserName;
+	private EditText edtxtPass;
+	private CheckBox chBoxSave;
+	private CheckBox chBoxHide;
+	private Button btnLogin;
+	private Button btnRegis;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		edtxtUserName = (EditText) findViewById(R.id.edtxt_username);
+		edtxtPass = (EditText) findViewById(R.id.edtxt_pass);
+		chBoxSave = (CheckBox) findViewById(R.id.chbox_save);
+		chBoxHide = (CheckBox) findViewById(R.id.chbox_hide);
+		btnLogin = (Button) findViewById(R.id.btn_login);
+		btnRegis = (Button) findViewById(R.id.btn_regis);
 
-    
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+	}
+
 }
