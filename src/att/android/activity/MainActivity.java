e87fa96206facc3fa1457dd19abb60b33f4b7252
hdtuan87@gmail.com <hdtuan87@gmail.com@cdc1b30c-ec19-abd6-4bf4-34af105c53e8>
2012-07-21 02:35:48 +0000

    update UI đã ngon mọi ng cho ý kiến đi nào

--- src/att/android/activity/MainActivity.java
+++ src/att/android/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends TabActivity {
@@ -36,5 +37,21 @@ public class MainActivity extends TabActivity {
 		mTabHost.addTab(tabLogin);
 		mTabHost.addTab(tabContactList);
 		mTabHost.addTab(tabReadRss);
+		
+		setBackgroundTab(mTabHost);
+		mTabHost.setOnTabChangedListener(new OnTabChangeListener() {
+
+	        public void onTabChanged(String arg0) {
+	        	setBackgroundTab(MainActivity.this.mTabHost);
+	        }
+	});
+	}
+	private void setBackgroundTab(TabHost mTabHost){
+		for(int i=0;i<mTabHost.getTabWidget().getChildCount();i++)
+		{   
+			mTabHost.getTabWidget().getChildAt(i).setBackgroundDrawable(getResources().getDrawable(R.drawable.tab_not_focus)); //unselected tab
+		}
+		
+		mTabHost.getTabWidget().getChildAt(mTabHost.getCurrentTab()).setBackgroundDrawable(getResources().getDrawable(R.drawable.tab_focus));
 	}
 }
