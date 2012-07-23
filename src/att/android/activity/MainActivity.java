03fcbb1656b669581f58c4e37c0fff4a8de32b24
hdtuan87 <hdtuan87@gmail.com>
2012-07-23 12:54:44 +0000

    update

--- src/att/android/activity/MainActivity.java
+++ src/att/android/activity/MainActivity.java
@@ -3,14 +3,19 @@ package att.android.activity;
 import com.example.multiapp.R;
 
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends TabActivity {
 	private TabHost mTabHost;
+	private TabSpec tabReadRss;
+	private TabSpec tabWebView;
+	private View tab;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +34,22 @@ public class MainActivity extends TabActivity {
 		Intent itContact = new Intent(this, ContactListActivity.class);
 		tabContactList.setContent(itContact);
 		
-		TabSpec tabReadRss = mTabHost.newTabSpec("tabReadRss");
+		tabReadRss = mTabHost.newTabSpec("tabReadRss");
 		tabReadRss.setIndicator("News");
 		Intent itReadRss = new Intent(this, RssActivity.class);
 		tabReadRss.setContent(itReadRss);
 		
+		tabWebView = mTabHost.newTabSpec("Web");
+		tabWebView.setIndicator("Read News");
+		Intent itWebView = new Intent(this, WebViewActivity.class);
+		tabWebView.setContent(itWebView);
+		
 		mTabHost.addTab(tabLogin);
 		mTabHost.addTab(tabContactList);
 		mTabHost.addTab(tabReadRss);
+		mTabHost.addTab(tabWebView);
 		
+//		tab = getTabHost().getTabWidget().getChildTabViewAt(3);
 		setBackgroundTab(mTabHost);
 		mTabHost.setOnTabChangedListener(new OnTabChangeListener() {
 
@@ -57,4 +69,7 @@ public class MainActivity extends TabActivity {
 	public void switchTabSpecial(int tab){
         mTabHost.setCurrentTab(tab);
 }
+	public void changeTab(int tab){
+		
+	}
 }
