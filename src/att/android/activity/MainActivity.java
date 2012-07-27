0bcd043ade415e20331e4e077dca5d6d64d74f0b
hdtuan87@gmail.com <hdtuan87@gmail.com@cdc1b30c-ec19-abd6-4bf4-34af105c53e8>
2012-07-27 03:57:36 +0000

    Update UI Fragment. method nào yêu cầu Content thì mọi ng dùng hàm này để trả về Content nhá:  this.getActivity();

--- src/att/android/activity/MainActivity.java
+++ src/att/android/activity/MainActivity.java
@@ -1,78 +1,43 @@
 package att.android.activity;
 
-import android.app.TabActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TabHost;
-import android.widget.TabHost.OnTabChangeListener;
-import android.widget.TabHost.TabSpec;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import att.android.adapter.FixedTabsAdapter;
+import att.android.adapter.ViewPagerAdapter;
 
+import com.astuetz.viewpager.extensions.FixedTabsView;
+import com.astuetz.viewpager.extensions.TabsAdapter;
 import com.example.multiapp.R;
 
-public class MainActivity extends TabActivity {
-	private TabHost mTabHost;
+public class MainActivity extends FragmentActivity {
+
+	private ViewPager mPager;
+	private FixedTabsView mFixedTabs;
+
+	private ViewPagerAdapter mPagerAdapter;
+	private TabsAdapter mFixedTabsAdapter;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
-
-		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-
-		TabSpec tabLogin = mTabHost.newTabSpec("tabLogin");
-		tabLogin.setIndicator("", getResources().getDrawable(R.drawable.custom_login_tab));
-		Intent itLogin = new Intent(this, LoginActivity.class);
-		tabLogin.setContent(itLogin);
-
-		TabSpec tabContactList = mTabHost.newTabSpec("tabContact");
-		tabContactList.setIndicator("", getResources().getDrawable(R.drawable.custom_contact_tab));
-		Intent itContact = new Intent(this, ContactListActivity.class);
-		tabContactList.setContent(itContact);
-
-		TabSpec tabReadRss = mTabHost.newTabSpec("tabReadRss");
-		tabReadRss.setIndicator("", getResources().getDrawable(R.drawable.custom_rss_tab));
-		Intent itReadRss = new Intent(this, RssActivity.class);
-		tabReadRss.setContent(itReadRss);
-
-		TabSpec tabChatJoint = mTabHost.newTabSpec("chat");
-		tabChatJoint.setIndicator("", getResources().getDrawable(R.drawable.custom_chat_tab));
-		Intent itChatJoint = new Intent(this, ChatJointActivity.class);
-		tabChatJoint.setContent(itChatJoint);
-
-		mTabHost.addTab(tabLogin);
-		mTabHost.addTab(tabContactList);
-		mTabHost.addTab(tabChatJoint);
-		mTabHost.addTab(tabReadRss);
-
-		setBackgroundTab(mTabHost);
-		mTabHost.setOnTabChangedListener(new OnTabChangeListener() {
-
-			public void onTabChanged(String arg0) {
-				setBackgroundTab(MainActivity.this.mTabHost);
-			}
-		});
+		initViewPager(3, 0xFFFFFFFF, 0xFF000000);
+		mFixedTabs = (FixedTabsView) findViewById(R.id.fixed_tabs);
+		mFixedTabsAdapter = new FixedTabsAdapter(this);
+		mFixedTabs.setAdapter(mFixedTabsAdapter);
+		mFixedTabs.setViewPager(mPager);
 	}
 
-	private void setBackgroundTab(TabHost mTabHost) {
-//		for (int i = 0; i < mTabHost.getTabWidget().getChildCount(); i++) {
-//			mTabHost.getTabWidget()
-//					.getChildAt(i)
-//					.setBackgroundDrawable(
-//							getResources()
-//									.getDrawable(R.drawable.tab_not_focus));
-//		}
-//
-//		mTabHost.getTabWidget()
-//				.getChildAt(mTabHost.getCurrentTab())
-//				.setBackgroundDrawable(
-//						getResources().getDrawable(R.drawable.tab_focus));
+	private void initViewPager(int pageCount, int backgroundColor, int textColor) {
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPagerAdapter = new ViewPagerAdapter(getApplicationContext(),
+				getSupportFragmentManager());
+		mPager.setAdapter(mPagerAdapter);
+		mPager.setCurrentItem(1);
+		mPager.setPageMargin(1);
 	}
 
-	public void switchTabSpecial(int tab) {
-		mTabHost.setCurrentTab(tab);
-	}
-
-	public void changeTab(int tab) {
-
-	}
 }
