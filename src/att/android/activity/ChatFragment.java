c702d4706b721c5ff68e957b09620df9a3d5203a
slim.tbb@gmail.com <slim.tbb@gmail.com@cdc1b30c-ec19-abd6-4bf4-34af105c53e8>
2012-08-11 03:38:38 +0000

    Chat voi du lieu gia

--- src/att/android/activity/ChatFragment.java
+++ src/att/android/activity/ChatFragment.java
@@ -1,23 +1,43 @@
 package att.android.activity;
 
+import org.openymsg.network.Session;
+import org.openymsg.network.Status;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+import att.android.model.YGeneralHandler;
+import att.android.util.StartFragment;
 
 
 import com.example.multiapp.R;
 
 
-public class ChatFragment extends Fragment{
+public class ChatFragment extends Fragment implements OnClickListener, StartFragment{
 	private ImageView icon_status;
-	private Button btn_close;
+	private Button btn_back;
+	private Button btn_send;
+	private EditText edt_message;
 	private ScrollView scrollView;
+	private TextView friends_name;
+	private Status actionSttFriends;
+	private String YMuserID;
+	private LayoutInflater inflater;
+	private LinearLayout formchat;
+	Session instanceofSession = Session.getInstance();
+	YGeneralHandler sessionListenerInstance = YGeneralHandler.getInstance();
 	
 	public static Fragment newInstance(Context context) {
 		ChatFragment f = new ChatFragment();
@@ -32,10 +52,79 @@ public class ChatFragment extends Fragment{
 				null);
 		return root;
 	}
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		initComponent();
+		if(true){
+		receiveMessageProcess();
+		}
+	}
+	
+	private void initComponent() {
+		inflater = this.getLayoutInflater(getArguments());
+//		sttFriends = user.getStatus();
+		formchat = (LinearLayout) this.getView().findViewById(R.id.formchat_content);
+		icon_status = (ImageView) this.getView().findViewById(R.id.formchat_icon_status);
+		friends_name = (TextView) this.getView().findViewById(R.id.formchat_friends_name);
+		btn_send = (Button) this.getView().findViewById(R.id.formchat_btn_send);
+		btn_back = (Button) this.getView().findViewById(R.id.formchat_btn_back);
+		edt_message = (EditText) this.getView().findViewById(R.id.txt_formchat_message);
+		scrollView = (ScrollView) this.getView().findViewById(R.id.formchat_scrollbar);
+		scrollView.setVerticalScrollBarEnabled(false);
+		btn_send.setOnClickListener(this);
+		btn_back.setOnClickListener(this);
+		friends_name.setText("Somefriends here");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		instanceofSession.addSessionListener(sessionListenerInstance);
+	}
+	
+	public void onClick(View v) {
+		if (v == btn_send){
+			String msg = edt_message.getText().toString();
+			sendMessageProcess(msg);
+		}
+		if (v == btn_back){	
+			((StartFragment)getActivity()).startFragment(1);
+		}
+		
+	}
+
+	private void sendMessageProcess(String msg) {
+		View layout = inflater.inflate(R.layout.chatbox_me, null);
+		TextView me = (TextView) layout.findViewById(R.id.txt_chatbox_me);
+		ImageView avt_me = (ImageView) layout.findViewById(R.id.real_avatar_me);
+		me.setText(msg);
+		formchat.addView(layout);
+		scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+		scrollView.focusSearch(ScrollView.FOCUS_DOWN);
+	}
+	
+	private void receiveMessageProcess(){
+		View layout = inflater.inflate(R.layout.chatbox_myfriend, null);
+		TextView friends = (TextView) layout.findViewById(R.id.txt_chatbox_myfriend);
+		ImageView avt_friends = (ImageView) layout.findViewById(R.id.real_avatar_friend);
+		friends.setText("hello!");
+		formchat.addView(layout);
+		scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+		scrollView.focusSearch(ScrollView.FOCUS_DOWN);
+		int distance = scrollView.getBottom();
+		scrollView.scrollBy(0, distance);
+	}
+
+	public void startFragment(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+}
 	
 //		chatJointTabHost = (TabHost) this.findViewById(android.R.id.tabhost);
 //		arlTabspace = new ArrayList<TabHost.TabSpec>();
-	}
+	
 
 //	@Override
 //	public void onResume() {
