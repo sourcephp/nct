0de51beb732e52be3016145de4386f3906bbb2ed
hdtuan87@gmail.com <hdtuan87@gmail.com@cdc1b30c-ec19-abd6-4bf4-34af105c53e8>
2012-08-23 15:43:06 +0000

    check internet

--- src/att/android/activity/MainActivity.java
+++ src/att/android/activity/MainActivity.java
@@ -3,22 +3,29 @@ package att.android.activity;
 
 import android.app.AlertDialog;
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo.State;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import att.android.util.MyDialog;
+import att.android.util.MyDialog.OnMyDialogListener;
 
 import com.example.multiapp.R;
 
 public class MainActivity extends TabActivity {
+	
+	private MyDialog notRadio;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notRadio = new MyDialog(this, R.drawable.not_radio);
         TabHost mTabHost = getTabHost();
         
         TabSpec tabMess = mTabHost.newTabSpec("messenger");
@@ -32,9 +39,26 @@ public class MainActivity extends TabActivity {
         TabSpec tabRss = mTabHost.newTabSpec("rss");
         tabRss.setIndicator("", getResources().getDrawable(R.drawable.custom_rss_tab)).setContent(new Intent(this, RssActivity.class));
         mTabHost.addTab(tabRss);
-       
+       notRadio.setOnMyDialogListener(new OnMyDialogListener() {
+		
+		public void onItemClick(boolean isOk) {
+			if(!isOk){
+				MainActivity.this.finish();
+			}
+		}
+	});
+       checkInternet();
     }
+    private void checkInternet(){
+    	ConnectivityManager connec = (ConnectivityManager)
+    		    getSystemService(Context.CONNECTIVITY_SERVICE);
+    	if ( connec.getNetworkInfo(0).getState() == State.DISCONNECTED
+    		    ||  connec.getNetworkInfo(1).getState() == State.DISCONNECTED  )
+    		  {
+    		notRadio.show();
+    		  }
 
+    }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- src/att/android/util/MyDialog.java
+++ src/att/android/util/MyDialog.java
@@ -6,16 +6,20 @@ import android.content.Context;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MyDialog extends Dialog implements android.view.View.OnClickListener {
 	private Button btnOk;
 	private Button btnCancel;
+	private ImageView imgThumb;
 	private OnMyDialogListener mListener;
-	public MyDialog(Context context) {
+	public MyDialog(Context context, int thumb) {
 		super(context);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.my_dialog);
-		getWindow().setBackgroundDrawableResource(R.drawable.dialog_del);
+		getWindow().setBackgroundDrawableResource(R.drawable.dialog);
+		imgThumb = (ImageView) this.findViewById(R.id.img_thumb);
+		imgThumb.setBackgroundResource(thumb);
 		btnOk = (Button) this.findViewById(R.id.btn_ok_dialog);
 		btnOk.setOnClickListener(this);
 		btnCancel = (Button) this.findViewById(R.id.btn_cancel_dialog);
