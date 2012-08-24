6ee57382419545992b53c8e145c16f7c02fd9577
hdtuan87@gmail.com <hdtuan87@gmail.com@cdc1b30c-ec19-abd6-4bf4-34af105c53e8>
2012-08-24 18:57:06 +0000

    login fragment completed

--- src/att/android/fragment/LoginFragment.java
+++ src/att/android/fragment/LoginFragment.java
@@ -1,18 +1,22 @@
 package att.android.fragment;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.openymsg.network.AccountLockedException;
 import org.openymsg.network.FailedLoginException;
 import org.openymsg.network.LoginRefusedException;
 import org.openymsg.network.SessionState;
-import org.openymsg.network.Status;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,11 +28,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 import att.android.activity.MessengerFragmentActivity;
 import att.android.model.Logger;
+import att.android.util.DataUltils;
 
 import com.example.multiapp.R;
 
 public class LoginFragment extends BaseMessengerFragment implements
-		OnClickListener, Runnable {
+		OnClickListener {
 	private static final String TAG = "LoginFragment";
 	// ko hieu tai sao phai static moi chay duoc???
 	private static EditText edtxtUserName;
@@ -36,7 +41,8 @@ public class LoginFragment extends BaseMessengerFragment implements
 	private CheckBox chBoxSave;
 	private CheckBox chBoxHide;
 	private Button btnLogin;
-
+	private CheckAcc checkAcc;
+	
 	public static Fragment newInstance(Context context) {
 		LoginFragment f = new LoginFragment();
 
@@ -69,6 +75,28 @@ public class LoginFragment extends BaseMessengerFragment implements
 	@Override
 	public void initActions() {
 		btnLogin.setOnClickListener(this);
+		if(DataUltils.checkStoryFromInternalStorage("account.yahoo_id.bin", getActivity())){
+			String temp = getFile("account.yahoo_id.bin");
+			String[] acc = temp.split(",");
+			edtxtUserName.setText(acc[0]);
+			edtxtPass.setText(acc[1]);
+		}
+	}
+	private String getFile(String filePath){
+		InputStream is = DataUltils.openFileFromInternal(getActivity(), filePath);
+		Log.e("path----", filePath);
+		int i;
+		byte byt[] = new byte[1024];
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			for(; (i = is.read(byt))!= -1; ){
+				baos.write(byt,0,i);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		byte bis[] = Base64.decode(baos.toByteArray(), Base64.DEFAULT);
+		return new String(bis);
 	}
 
 	@Override
@@ -86,69 +114,66 @@ public class LoginFragment extends BaseMessengerFragment implements
 
 	public void onClick(View v) {
 		if (v == btnLogin) {
-			new Thread(new LoginFragment()).start();
-			//
-//			((MessengerFragmentActivity) this.getActivity()).startFragment(1);
+			String strUserName = edtxtUserName.getText().toString().trim();
+			String strPass = edtxtPass.getText().toString().trim();
+			String temp = strUserName + ","+strPass;
+			if(chBoxSave.isChecked()){
+			FileOutputStream fos = DataUltils.saveFileToInternalStorage(getActivity(), "account.yahoo", "id.bin");
+			byte[] byt = Base64.encode(temp.getBytes(), Base64.DEFAULT);
+			try {
+				fos.write(byt);
+				fos.flush();
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
+			if ("".equals(strUserName) || "".equals(strPass)) {
+				Toast.makeText(getActivity(),
+						"Ten tai khoan va password khong duoc de trong!",
+						Toast.LENGTH_LONG).show();
+			} else{
+				checkAcc = new CheckAcc();
+				checkAcc.execute(strUserName, strPass);
+			}
 		}
-
 	}
 
-	public void run() {
+	private class CheckAcc extends AsyncTask<String, String, Boolean> {
 
-		String strUserName = edtxtUserName.getText().toString().trim();
-		String strPass = edtxtPass.getText().toString().trim();
-		Handler handler = new Handler(Looper.getMainLooper());
-
-		// boolean bSave = chBoxSave.hasSelection();
-		// boolean bHide = chBoxHide.hasSelection();
-
-		if ("".equals(strUserName) || "".equals(strPass)) {
-			handler.post(new Runnable() {
-			    public void run()
-			    {
-			    	Toast.makeText(getActivity(), "Ten tai khoan va password khong duoc de trong!", Toast.LENGTH_LONG).show();
-			    }
-			});
-			
-		} else {
-			Log.i(TAG, "Login start");
+		@Override
+		protected Boolean doInBackground(String... params) {
+			boolean idTrue = true;
+			String strUserName = params[0];
+			String strPass = params[1];
 			try {
 				singletonSession.login(strUserName, strPass);
-				singletonSession.setStatus(Status.AVAILABLE);
+				if(chBoxHide.isChecked()){
+					singletonSession
+					.setStatus(org.openymsg.network.Status.INVISIBLE);
+				}else
+				singletonSession
+						.setStatus(org.openymsg.network.Status.AVAILABLE);
 			} catch (AccountLockedException e) {
-				handler.post(new Runnable() {
-				    public void run()
-				    {
-				    	Toast.makeText(getActivity(), "Tai khoan da bi khoa!", Toast.LENGTH_LONG).show();
-				    }
-				});
-				
+				idTrue = false;
+				publishProgress("Tai khoan da bi khoa!");
 				e.printStackTrace();
 			} catch (IllegalStateException e) {
-				handler.post(new Runnable() {
-				    public void run()
-				    {
-				    	Toast.makeText(getActivity(), "Thong tin dang nhap ko dung!", Toast.LENGTH_LONG).show();
-				    }
-				});
+				idTrue = false;
+				publishProgress("Thong tin dang nhap ko dung!");
 				e.printStackTrace();
 			} catch (LoginRefusedException e) {
-				handler.post(new Runnable() {
-				    public void run()
-				    {
-				    	Toast.makeText(getActivity(), "Thong tin dang nhap ko dung!", Toast.LENGTH_LONG).show();
-				    }
-				});
+				idTrue = false;
+				Log.d(TAG, "IO error");
+				publishProgress("Thong tin dang nhap ko dung!");
 				e.printStackTrace();
 			} catch (FailedLoginException e) {
-				handler.post(new Runnable() {
-				    public void run()
-				    {
-				    	Toast.makeText(getActivity(), "Thong tin dang nhap ko dung!", Toast.LENGTH_LONG).show();
-				    }
-				});
+				idTrue = false;
+				publishProgress("Thong tin dang nhap ko dung!");
 				e.printStackTrace();
 			} catch (IOException e) {
+				idTrue = false;
 				Log.d(TAG, "IO error");
 				e.printStackTrace();
 			}
@@ -167,7 +192,27 @@ public class LoginFragment extends BaseMessengerFragment implements
 					Logger.e(TAG, "Login data loaded");
 				}
 			}
+			return idTrue;
 		}
-	}
 
+		@Override
+		protected void onProgressUpdate(String... values) {
+			super.onProgressUpdate(values);
+			Toast.makeText(getActivity(), values[0], Toast.LENGTH_LONG).show();
+			this.cancel(true);
+		}
+
+		@Override
+		protected void onCancelled() {
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			super.onPostExecute(result);
+			((MessengerFragmentActivity) LoginFragment.this.getActivity())
+					.startFragment(1);
+		}
+
+	}
 }
